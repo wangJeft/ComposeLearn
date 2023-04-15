f3b690b7afc21b09cdb62e274eff43bb07b1101d
jeft <jeftwang@foxmail.com>
2023-04-15 16:25:39 +0800

    lazy column

---
+++ app/src/main/java/com/jeft/composelearn/basicComponents/chpater2/TestList.kt
@@ -0,0 +1,47 @@
+package com.jeft.composelearn.basicComponents.chpater2
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TestList() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray),
+        contentPadding = PaddingValues(35.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items((1..50).toList()) { index ->
+            ContentCard(index = index)
+        }
+    }
+}
+
+@Composable
+fun ContentCard(index: Int) {
+    Card(elevation = 8.dp, modifier = Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp), contentAlignment = Alignment.Center
+        ) {
+            Text(text = "第${index}卡片", style = MaterialTheme.typography.h5)
+        }
+    }
+}
--- app/src/main/java/com/jeft/composelearn/basicComponents/chpater2/TestScaffold.kt
+++ app/src/main/java/com/jeft/composelearn/basicComponents/chpater2/TestScaffold.kt
@@ -1,8 +1,11 @@
 package com.jeft.composelearn.basicComponents.chpater2
 
 import android.annotation.SuppressLint
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -10,13 +13,13 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import com.jeft.composelearn.R
+import kotlinx.coroutines.launch
 
 
 data class Item(val name: String, val icon: Int)
@@ -25,7 +28,7 @@ data class Item(val name: String, val icon: Int)
 @Composable
 fun TestScaffold() {
 
-    var selectedItem by remember {
+    val selectedItem by remember {
         mutableStateOf(0)
     }
     val items = listOf<Item>(
@@ -34,19 +37,40 @@ fun TestScaffold() {
         Item("设置", R.drawable.setting)
     )
 
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "主页") }, navigationIcon = {
             IconButton(
-                onClick = { },
+                onClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                },
             ) {
                 Icon(Icons.Filled.Menu, contentDescription = "")
             }
         })
-    }) {
+    }, bottomBar = {
+        BottomNavigation {
+            items.forEachIndexed { index, item ->
+                BottomNavigationItem(selected = selectedItem == index, onClick = { }, icon = {
+                    Icon(
+                        painter = painterResource(id = item.icon), contentDescription = item.name
+                    )
+                }, alwaysShowLabel = false, label = { Text(text = item.name) })
+            }
+        }
+    }, drawerContent = { Text(text = "侧边栏") }, scaffoldState = scaffoldState) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             Text(text = "主页界面")
-
         }
 
+    }
+    BackHandler(enabled = scaffoldState.drawerState.isOpen) {
+        scope.launch {
+            scaffoldState.drawerState.close()
+        }
     }
 }
